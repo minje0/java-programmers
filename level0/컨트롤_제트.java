22597546c82bc5a7fba95857bbd75b50f1af7c04
minje <alswp0910@naver.com>
2022-11-25 22:23:36 +0900

    컨트롤 제트

--- level0/컨트롤_제트.java
+++ level0/컨트롤_제트.java
@@ -1,25 +1,26 @@
 package level0;
 
+import java.util.Stack;
+
 public class 컨트롤_제트 {
     public static void main(String[] args) {
-        String s = "1 1 Z Z";
+        String s = 	"10 Z 20 Z 1";
+        //String s = "1 1 Z Z";
         System.out.println(solution(s));
     }
     public static int solution(String s) {
         int answer = 0;
-        String [] str = s.split("\\s");
+        String[] str = s.split("\\s");
+        Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < str.length; i++) {
-            if(str[i].equals("Z")){
-                if (i == 0) {
-                    continue;
-                }
-                else if(str[i-1].matches("-?\\d+")) {
-                    answer -= Integer.parseInt(str[i-1]);
-                }
-            }else{
-                answer += Integer.parseInt(str[i]);
+            if (str[i].equals("Z") && !stack.empty()) {
+                    stack.pop();
+            } else {
+                stack.push(Integer.parseInt(str[i]));
             }
-
+        }
+        for (int i : stack) {
+            answer += i;
         }
         return answer;
     }
